bde676c85cc805d0f7861956ee4e24a9a4c8e02d
Laurence <92445633+MistySorrow@users.noreply.github.com>
2021-10-27 14:22:26 +0000

    poisson 1 a,b fertig

--- poisson.cc
+++ poisson.cc
@@ -4,17 +4,22 @@
 
 
 double poisson(double mu, int k) {
-    return 0;
+  return 0;
 }
 
 int main() {
-    using namespace std;
-
-
-    ifstream fin("datensumme.txt");
-    int n_i;
-    for(int i = 0 ; i < 234 ; ++i) {
-        fin >> n_i;
-    }
-    fin.close();
+  std::ifstream fin("datensumme.txt");
+  int n_i;
+  std::vector<int> zaehler(11);
+  for(int i = 0 ; i < 234 ; ++i) {
+    fin >> n_i;
+    zaehler[n_i] += 1;
+  }
+  fin.close();
+  std::ofstream fout("hist.txt");
+  for (unsigned int k = 0; k < zaehler.size(); k++) {
+    std::cout << k << ":" << zaehler[k] << std::endl;
+    fout << k << " " << zaehler[k] << std::endl;
+  }
+  fout.close();
 }
